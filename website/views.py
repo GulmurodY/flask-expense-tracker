b459fb6f71bf1511f33763c5787d0157602c6ed6
Yorov Gulmurod <113702748+GulmurodY@users.noreply.github.com>
2024-03-27 15:57:10 +0300

    Update views.py

--- website/views.py
+++ website/views.py
@@ -32,7 +32,7 @@ def home():
             new_note = Note(user_id=current_user.id, amount=amount, type=type, comment=comment)
             db.session.add(new_note)
             db.session.commit()
-            flash('Note added!', category='success')
+            flash('Expense added!', category='success')
 
     return render_template("home.html", user=current_user)
 
